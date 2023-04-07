e900c1e8f60d1827e525e23210a9de0b55f697e8
Ali Abbas Malik <aliabbas.malik.fl@crowdbotics.com>
2023-04-07 15:14:53 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "demo-2-39773",
   "url": "https://demo-2-39773.botics.co",
   "api": "https://demo-2-39773.botics.co/api/v1",
-  "initialRoute": "Home324891295837_3_2489"
+  "initialRoute": "TicketSelection(91:1017)1295836_91_1017"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
